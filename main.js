9043ed7f029eb657a4602dd3071cdef6150c708d
daniellasilva.design <daniellasilva.design@gmail.com>
2024-08-31 16:39:51 -0600

    Se agregó Blog section

--- main.js
+++ main.js
@@ -18,3 +18,15 @@ document.getElementById('form')
       alert(JSON.stringify(err));
     });
 });
+
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+    event.addEventListener("click", () => {
+        if (event.classList.contains("active")) {
+            event.classList.remove("active");
+        } else {
+            event.classList.add("active");
+        }
+    });
+});
